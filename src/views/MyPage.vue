5f3b4ea4a9a87b8ddecd837b375905e927b71904
김준영 <novb1492@naver.com>
2022-07-27 20:15:19 +0900

    test

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -47,10 +47,10 @@ const getCredentialDefaultArgs = {
 };
 export default {
     methods:{
-        upsertLocalLogin(){
+      async  upsertLocalLogin(){
             //지문 등록 할때는 create
             // register / create a new credential
-            navigator.credentials.create(createCredentialDefaultArgs)
+          await navigator.credentials.create(createCredentialDefaultArgs)
                 .then((cred) => {
                 console.log("NEW CREDENTIAL", cred);
                 const idList = [{
@@ -66,7 +66,7 @@ export default {
                 console.log("ERROR", err);
             });
              //지문으로 로그인 할때는 get
-            navigator.credentials.get(getCredentialDefaultArgs).then((assertion) => {
+          await  navigator.credentials.get(getCredentialDefaultArgs).then((assertion) => {
                 this.assertions = assertion;
                 alert(assertion.id);
                 console.log("ASSERTIONs", assertion);
