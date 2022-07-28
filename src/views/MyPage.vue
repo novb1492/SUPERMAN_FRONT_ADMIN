789d588f3c4a1c88e52f9be120b67da5d9aa257d
Enigma <74756147+novb1492@users.noreply.github.com>
2022-07-28 11:19:15 +0900

    test

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -2,6 +2,7 @@
   <div style="margin-top: 100px;">
     <button @click="upsertLocalLogin">생체 인증 수단 등록/수정</button>
   </div>
+  {{creds}}
 </template>
 
 <script>
@@ -29,7 +30,7 @@ const createCredentialDefaultArgs = {
     timeout: 60000,
 
     challenge: new Uint8Array([ // must be a cryptographically random number sent from a server
-      0x8C, 0x0A, 0x26, 0xFF, 0x22, 0x91, 0xC1, 0xE9, 0xB9, 0x4E, 0x2E, 0x17, 0x1A, 0x98, 0x6A, 0x73,
+      0, 1, 2,3, 4, 5, 6, 7, 0xB9, 0x4E, 0x2E, 0x17, 0x1A, 0x98, 0x6A, 0x73,
       0x71, 0x9D, 0x43, 0x48, 0xD5, 0xA7, 0x6A, 0x15, 0x7E, 0x38, 0x94, 0x52, 0x77, 0x97, 0x0F, 0xEF
     ]).buffer
   }
@@ -46,6 +47,11 @@ const getCredentialDefaultArgs = {
   },
 };
 export default {
+    data() {
+        return {
+            creds:null
+        }
+    },
     methods:{
         upsertLocalLogin(){
             //지문 등록 할때는 create
@@ -53,6 +59,7 @@ export default {
             navigator.credentials.create(createCredentialDefaultArgs)
                 .then((cred) => {
                 console.log("NEW CREDENTIAL", cred);
+                this.creds=cred.response;
                 const idList = [{
                     id: cred.rawId,
                     transports: ["usb", "nfc", "ble"],
