1a9e5eac89c697718427045c856f017475a18e96
김준영 <novb1492@naver.com>
2022-08-14 16:14:47 +0900

    스토어 네비바 생성

---
+++ src/components/layout/storeManu/OrderNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="javascript:void();" id="navbarDropdown" role="button"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            주문관리
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="javascript:void();" @click="goRegiStore">완료된주문</a></li>
+            <li><a class="dropdown-item" href="javascript:void();" @click="goStoreList">들어온주문</a></li>
+            <li><a class="dropdown-item" href="javascript:void();" @click="goStoreList">취소된주문</a></li>
+        </ul>
+    </li>
+</template>
+
+<script>
+
+export default {
+    name: 'OrderNav',
+}
+</script>
---
+++ src/components/layout/storeManu/ProductNav.vue
@@ -0,0 +1,19 @@
+<template>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="javascript:void();" id="navbarDropdown" role="button"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            상품관리
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="javascript:void();" @click="goRegiStore">상품등록</a></li>
+            <li><a class="dropdown-item" href="javascript:void();" @click="goStoreList">상품조회</a></li>
+        </ul>
+    </li>
+</template>
+
+<script>
+
+export default {
+    name: 'ProductNav',
+}
+</script>
