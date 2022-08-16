3c2806e94c71a89b926a56b5e9fef3258b53b261
Enigma <74756147+novb1492@users.noreply.github.com>
2022-08-16 17:55:50 +0900

    스토어 네비에 직원조회 추가

--- src/components/layout/admin/AdminStoreNav.vue
+++ src/components/layout/admin/AdminStoreNav.vue
@@ -3,6 +3,7 @@
     <OrderNav></OrderNav>
     <DeliverNav></DeliverNav>
     <ProductNav></ProductNav>
+    <EmployeeNav></EmployeeNav>
     <SalesNav></SalesNav>
 </template>
 
@@ -12,8 +13,9 @@ import DeliverNav from "@/components/layout/storeMenu/DeliverNav.vue";
 import SalesNav from "@/components/layout/storeMenu/SalesNav.vue";
 import ShowState from '../ShowState.vue';
 import ProductNav from '../storeMenu/ProductNav.vue';
+import EmployeeNav from '../mainMenu/EmployeeNav.vue';
 export default {
     name: "AdminStoreNav",
-    components: { OrderNav, DeliverNav, SalesNav, ShowState, ProductNav }
+    components: { OrderNav, DeliverNav, SalesNav, ShowState, ProductNav, EmployeeNav }
 }
 </script>
--- src/components/layout/user/UserStoreNav.vue
+++ src/components/layout/user/UserStoreNav.vue
@@ -3,6 +3,7 @@
     <OrderNav></OrderNav>
     <DeliverNav></DeliverNav>
     <ProductNav></ProductNav>
+    <EmployeeNav></EmployeeNav>
 </template>
 
 <script>
@@ -10,8 +11,9 @@ import OrderNav from '@/components/layout/storeMenu/OrderNav.vue';
 import DeliverNav from "@/components/layout/storeMenu/DeliverNav.vue";
 import ShowState from '../ShowState.vue';
 import ProductNav from '../storeMenu/ProductNav.vue';
+import EmployeeNav from '../mainMenu/EmployeeNav.vue';
 export default {
     name: "ManageStoreNav",
-    components: { OrderNav, DeliverNav, ShowState, ProductNav }
+    components: { OrderNav, DeliverNav, ShowState, ProductNav, EmployeeNav }
 }
 </script>
