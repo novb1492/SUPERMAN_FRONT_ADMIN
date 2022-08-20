c63569bffc90c7791a5c94be92ee4e71051206c4
Enigma <74756147+novb1492@users.noreply.github.com>
2022-08-20 11:34:49 +0900

    머지누락  (#21)
    * 메인 페이지시 직원조회 숨기기
    
    * 컴포넌트 재랜더링  key로 바꿔주면됨
    
    * 버그 fix

---
+++ src/components/company/CompanyDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="comapnyNumArea">
+      <span>사업자번호</span>
+      <input type="text" v-model="companynum" :disabled="dFlag">
+    </div>
+    <div>
+      <span>대표자명</span>
+      <input type="text" v-model="name" :disabled="dFlag">
+    </div>
+    <div>
+      <span>개업일</span>
+      <input type="date" name="" id="" v-model="date" :disabled="dFlag">
+    </div>
+    <button :hidden="flag||!dFlag" @click="changeDisabled" >수정</button>
+</template>
+
+<script>
+export default {
+    props: ['companyinfo','flag'],
+    data() {
+    return {
+      companynum: this.companyinfo.num,
+      name: this.companyinfo.name,
+      date: this.companyinfo.openDate,
+      dFlag:true
+    }
+  },
+  methods:{
+    changeDisabled(){
+        this.dFlag=false;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/company/CompanyListPage.vue
@@ -0,0 +1,88 @@
+<template>
+    <div style="margin-top: 70px;">
+        <div v-for="(company, index) in infoList" :key="index" >
+            <CompanyDetail :companyinfo="company" :flag="false" :key="company.id"></CompanyDetail>
+        </div>
+        <div class="pagingContainer">
+            <div class="pagingbox">
+                <button @click="nextStore(1)" :disabled="last">다음</button>
+                <button @click="nextStore(-1)" :disabled="first">이전</button>
+            </div>
+            <div class="pagingbox">
+                {{ nowPage }}/{{ totalPage }}
+            </div>
+            <div class="pagingbox">
+                <select name="pets" id="pet-select" v-model="category">
+                    <option value="name">이름</option>
+                </select>
+                <input type="text" v-model="keyword">
+                <input type="button" value="검색" @click="search" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import CompanyDetail from '@/components/company/CompanyDetail.vue'
+import { mapGetters } from 'vuex';
+import { checkPage } from '@/assets/js/Jslib';
+
+export default {
+    components: { CompanyDetail },
+    watch: {
+        '$route'() {
+            this.requestGet();
+
+        }
+    },
+    data() {
+        return {
+            category: null,
+            keyword: null,
+            componentKey: 0
+        }
+    },
+    computed: {
+        ...mapGetters('basicStore', {
+            infoList: 'getInfoList',
+            last: 'getLast',
+            first: 'getFirst',
+            nowPage: 'getNowPage',
+            totalPage: 'getTotalPage'
+        })
+    },
+    mounted() {
+        this.$store.dispatch('NavStore/changeSituation', 0);
+        this.requestGet();
+
+    },
+    methods: {
+        changeKey(num) {
+            this.key = num + this.key;
+        },
+        requestGet() {
+            let keyword = this.$route.query.keyword;
+            let category = this.$route.query.category;
+            let page = checkPage(this.$route.query.page);
+            let url = '/admin/company/list?page=' + page + '&keyword=' + keyword + '&category=' + category;
+            this.$store.dispatch('basicStore/getInfolist', { url: url });
+        },
+        nextStore(num) {
+            let page = (this.$route.query.page * 1) + num;
+            let keyword = this.$route.query.keyword;
+            let category = this.$route.query.category;
+            this.changeUrl(page, keyword, category);
+        },
+        search() {
+            this.changeUrl(1, this.keyword, this.category);
+        },
+        changeUrl(page, keyword, category) {
+            let changeUrl = '/company-list?page=' + page + '&keyword=' + keyword + '&category=' + category;
+            this.$router.push(changeUrl);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
