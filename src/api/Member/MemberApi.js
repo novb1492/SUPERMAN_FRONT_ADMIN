f35509178e972520750724ae8506eb9bd6025ca8
김준영 <novb1492@naver.com>
2022-09-01 21:07:47 +0900

    로그아웃 추가

--- src/api/Member/MemberApi.js
+++ src/api/Member/MemberApi.js
@@ -4,7 +4,12 @@ function requestSearchMember(userId) {
     setInterceptors(instance);
     return  instance.get('/manage/member/info/'+userId);
 }
+function requestLogOut() {
+    setInterceptors(instance);
+    return  instance.post('/log-out',null);
+}
 export {
-    requestSearchMember
+    requestSearchMember,
+    requestLogOut
   
 }
--- src/components/layout/ShowState.vue
+++ src/components/layout/ShowState.vue
@@ -4,6 +4,8 @@
 </template>
 
 <script>
+import { requestLogOut } from '@/api/Member/MemberApi';
+import { checkNew, errorHandle } from '@/assets/js/Jslib';
 import { mapGetters } from 'vuex';
 
 export default {
@@ -17,8 +19,24 @@ export default {
             location.href = '/login';
         },
         logOut() {
+            requestLogOut().then(()=>{
+                this.doneLogOut();
+            }).catch(error=>{
+                let response = error.response;
+                let data = response.data;
+                if (checkNew(response.status, data.message)) {
+                    requestLogOut().then(() => {
+                        this.doneLogOut();
+                    }).catch(error => {
+                        errorHandle(error);
+                    })
+                } else {
+                    errorHandle(error);
+                }
+            })
+        },
+        doneLogOut(){
             this.$store.dispatch('NavStore/changeLoginFlag', false);
-            localStorage.removeItem('authentication');
             localStorage.removeItem('info');
             this.$router.go(0);
         }
